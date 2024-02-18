da978e87763845008850d6a95bb4b2a0cce84b8e
angieEncoded <43992078+angieEncoded@users.noreply.github.com>
2024-02-18 09:45:40 -0500

    Draw call to triangle

--- OpenGL_Reference/src/Application.cpp
+++ OpenGL_Reference/src/Application.cpp
@@ -28,6 +28,20 @@ int main(void) {
     }
 
     std::cout << glGetString(GL_VERSION) << std::endl;
+    // Drawing a basic triangle
+    //================================================================
+    float positions[6] = {
+       -0.5f,   -0.5f,
+        0.0f,    0.5f,
+        0.5f,   -0.5f
+    };
+
+    unsigned int buffer; // it wants an int to write its data into
+    glGenBuffers(1, &buffer); // puts in the ID of the generated buffer, the function returns void so we have to have somewhere to store it
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); // bind (select) the buffer, now we are ready to work with the buffer
+                // target           size            where       what to do
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    //================================================================
 
 
     /* Loop until the user closes the window */
@@ -36,14 +50,15 @@ int main(void) {
         glClear(GL_COLOR_BUFFER_BIT);
         
 
-        // Legacy way to create a basic triangle
-        glBegin(GL_TRIANGLES);
-            glVertex2f(-0.5f, -0.5f);
-            glVertex2f(0.0f, 0.5f);
-            glVertex2f(0.5f, -0.5f);
-            glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3); // the draw call
+        
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
+
+
+
 
         /* Poll for and process events */
         glfwPollEvents();
